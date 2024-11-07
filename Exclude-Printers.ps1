7cdacf5c69a12ad2ff2b88561329ea9440978bd6
Lucas Reis <82230039+LuksReis@users.noreply.github.com>
2024-11-07 07:50:17 -0300

    Add files via upload

---
+++ Exclude-Printers.ps1
@@ -0,0 +1,31 @@
+﻿# Created by: Lucas Reis - lucas.reis@syngenta.com
+# ---- Exclude printers
+
+$Printer = Get-Printer | where {
+    (
+    $_.Name -NotMatch "PullPrint" -and
+    $_.Name -NotMatch "IMP-TORRE02-COLORIDA-ADM" -and
+    $_.Name -NotMatch "IMP-TORRE02-COLORIDA-PRODUCAO" -and
+    $_.Name -NotMatch "Fax" -and
+    $_.Name -NotMatch "Microsoft Print to PDF" -and
+    $_.Name -NotMatch "Microsoft XPS Document Writer" -and
+    $_.Name -NotMatch "OneNote (Desktop)" -and
+    $_.Name -NotMatch "OneNote for Windows 10" -and
+    $_.DriverName -NotMatch "Zebra" -and
+    $_.DriverName -NotMatch "Zdesigner" -and
+    $_.DriverName -NotMatch "Intermec"
+    )
+}
+
+if($Printer){Remove-Printer -InputObject $Printer}
+
+ETIQUETA-TORRE02-ADM
+ETIQUETA-TORRE02-PRODUCAO
+
+IMP-TORRE02-COLORIDA-ADM
+IMP-TORRE02-COLORIDA-PRODUCAO
+
+
+$Printer = Get-Printer
+
+$Printer.DriverName -match "PCL"
